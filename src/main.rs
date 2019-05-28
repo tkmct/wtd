7da071a9f83b1e606d01ce6eb858d150e917242a
tkmct <1220t.takamichi@gmail.com>
2019-05-28 15:18:08 +0900

    shell loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+use std::io::prelude::*;
+
+fn handle(input: String) {
+    match input {
+        _ => println!("No command matched for input. Please input help to see available command."),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    loop {
+        print!("wtd> ");
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
+        match stdin.lock().read_line(&mut input) {
+            Ok(_) => handle(input),
+            Err(error) => println!("error: {}", error),
+        }
+    }
 }
